616b8e86347e6b713983f4ec5d57c81e072dfe3b
Zlatko <zlatko.stapic@foi.hr>
2018-10-23 08:09:06 +0200

    02 Add constructors, getters and seters  - add empty constructor (needed for dbflow)  - add parametrized constructor  - add getters and setters

--- database/src/main/java/air18/foi/hr/database/entities/Discount.java
+++ database/src/main/java/air18/foi/hr/database/entities/Discount.java
@@ -10,4 +10,73 @@ public class Discount {
     Date startDate; // java.util.Date
     Date endDate;
     int discount;
+
+    public Discount() {
+    }
+
+    public Discount(int id, String name, String description, int storeId, Date startDate, Date endDate, int discount) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.storeId = storeId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
 }
