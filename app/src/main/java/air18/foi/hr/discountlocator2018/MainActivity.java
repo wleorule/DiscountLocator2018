ba76025d569a4491dde87f26ce75b9e2590dc547
Zlatko <zlatko.stapic@foi.hr>
2018-10-23 08:36:11 +0200

    02 Refactor to log the data  - add new static method in main database  - mock data if database is empty  - log existing data  - refactor main activity

--- app/src/main/java/air18/foi/hr/discountlocator2018/MainActivity.java
+++ app/src/main/java/air18/foi/hr/discountlocator2018/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         MainDatabase.initDatabase(this);
-        MockData.writeAll();
+        MainDatabase.mockAndLogData();
     }
 }
--- database/src/main/java/air18/foi/hr/database/MainDatabase.java
+++ database/src/main/java/air18/foi/hr/database/MainDatabase.java
@@ -5,6 +5,14 @@ import android.content.Context;
 import com.raizlabs.android.dbflow.annotation.Database;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.util.List;
+
+import air18.foi.hr.database.data.MockData;
+import air18.foi.hr.database.entities.Discount;
+import air18.foi.hr.database.entities.Discount_Table;
+import air18.foi.hr.database.entities.Store;
 
 @Database(name = MainDatabase.NAME, version = MainDatabase.VERSION)
 public class MainDatabase {
@@ -14,5 +22,18 @@ public class MainDatabase {
     public static void initDatabase(Context context)
     {
         FlowManager.init(new FlowConfig.Builder(context).build());
+    }
+
+    public static void mockAndLogData()
+    {
+        if(SQLite.select().from(Store.class).queryList().isEmpty()){
+            MockData.writeAll();
+        }
+
+        List<Discount> discounts = SQLite.select().from(Discount.class).where(Discount_Table.discount.greaterThan(5)).queryList();
+
+        for(Discount discount : discounts){
+            System.out.println("Discount: " + discount.getName());
+        }
     }
 }
