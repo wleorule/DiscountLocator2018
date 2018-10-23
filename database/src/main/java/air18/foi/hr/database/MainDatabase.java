440edca58dba54c8afbf1a30a568e9671ab463c0
Zlatko <zlatko.stapic@foi.hr>
2018-10-23 08:14:39 +0200

    02 Add main database class  - define database name  - define database version

---
+++ database/src/main/java/air18/foi/hr/database/MainDatabase.java
@@ -0,0 +1,9 @@
+package air18.foi.hr.database;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+@Database(name = MainDatabase.NAME, version = MainDatabase.VERSION)
+public class MainDatabase {
+    public static final String NAME = "main";
+    public static final int VERSION = 1;
+}
