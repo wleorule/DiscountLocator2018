36ff9d5ed8e245440ffb9ca9a483d1a730ebb443
Boris Tomas <boris.tomas@foi.hr>
2018-10-28 15:43:31 +0100

    03 MainActivity made listener for loaded data  - added DataLoadedListener interface implementation

--- app/src/main/java/air18/foi/hr/discountlocator/MainActivity.java
+++ app/src/main/java/air18/foi/hr/discountlocator/MainActivity.java
@@ -6,12 +6,18 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.air.ws.core.DataLoadedListener;
+
+import java.util.ArrayList;
+
 import air18.foi.hr.database.MainDatabase;
+import air18.foi.hr.database.entities.Discount;
+import air18.foi.hr.database.entities.Store;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DataLoadedListener {
 
     @BindView(R.id.discount_list)
     ListView mListView;
@@ -28,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.test_button)
     public void buttonClicked(View view){
         final String[] listItems = MainDatabase.getDiscounts();
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDataLoaded(ArrayList<Store> stores, ArrayList<Discount> discounts) {
+        System.out.println("Data is here... ");
+        String[] listItems = new String[discounts.size()];
+
+        for (int i = 0; i < discounts.size(); i++) {
+            listItems[i] = discounts.get(i).getName();
+        }
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
         mListView.setAdapter(adapter);
