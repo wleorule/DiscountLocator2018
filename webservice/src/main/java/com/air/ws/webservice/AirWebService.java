2de0741c6f364142bb4db9b68ab747a8378baec1
Boris Tomas <boris.tomas@foi.hr>
2018-10-28 14:01:44 +0100

    03 Web service caller  - implemented constructor  - added getAll method stub

--- webservice/src/main/java/com/air/ws/webservice/AirWebService.java
+++ webservice/src/main/java/com/air/ws/webservice/AirWebService.java
@@ -1,4 +1,18 @@
 package com.air.ws.webservice;
 
+import com.air.ws.webservice.responses.AirWebServiceResponse;
+
+import retrofit.Call;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
 public interface AirWebService {
+    @FormUrlEncoded
+    @POST("stores.php")
+    Call<AirWebServiceResponse> getStores(@Field("method") String method);
+
+    @FormUrlEncoded
+    @POST("discounts.php")
+    Call<AirWebServiceResponse> getDiscounts(@Field("method") String method);
 }
--- webservice/src/main/java/com/air/ws/webservice/AirWebServiceCaller.java
+++ webservice/src/main/java/com/air/ws/webservice/AirWebServiceCaller.java
@@ -1,4 +1,38 @@
 package com.air.ws.webservice;
 
+import com.squareup.okhttp.OkHttpClient;
+
+import java.lang.reflect.Type;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
 public class AirWebServiceCaller {
+    // retrofit object
+    Retrofit retrofit;
+    // base URL of the web service
+    private final String baseUrl = "http://cortex.foi.hr/mtl/courses/air/";
+
+    // constructor
+    public AirWebServiceCaller(){
+
+        OkHttpClient client = new OkHttpClient();
+
+        // for debuggint use HttpInterceptor
+        //client.interceptors().add(new HttpInterceptor());
+
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+    }
+
+    // get all records from a web service
+    public void getAll(String method, final Type entityType){
+
+        //todo: implement web service call and response handling
+
+    }
 }
