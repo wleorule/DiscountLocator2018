6522517946ef4f29662a9ce096048671a9a33c32
Mišo <mdzeko@foi.hr>
2018-11-05 14:05:56 +0100

    04 Added main menu on main activity. Ready for shared pref + FIX
    - strings for the last commit were mistakenly put into database module
    strings.xml instead of app module strings.xml

--- app/src/main/java/air18/foi/hr/discountlocator/MainActivity.java
+++ app/src/main/java/air18/foi/hr/discountlocator/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -66,5 +69,18 @@ public class MainActivity extends AppCompatActivity implements DataLoadedListene
                 mRecycler.setLayoutManager(new LinearLayoutManager(this));
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO: code to be added here
+        return super.onOptionsItemSelected(item);
     }
 }
