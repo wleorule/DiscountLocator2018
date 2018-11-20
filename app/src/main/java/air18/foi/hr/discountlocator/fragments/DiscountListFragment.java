80541207dbc709ac859b09d30bf4b07bf1c9fc1c
Zlatko <zlatko.stapic@foi.hr>
2018-11-20 10:51:20 +0100

    05 Discount list: create new layout file and use it in fragment

--- app/src/main/java/air18/foi/hr/discountlocator/fragments/DiscountListFragment.java
+++ app/src/main/java/air18/foi/hr/discountlocator/fragments/DiscountListFragment.java
@@ -8,11 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import air18.foi.hr.discountlocator.R;
+
 public class DiscountListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_discount_list, container, false);
     }
 
     @Override
