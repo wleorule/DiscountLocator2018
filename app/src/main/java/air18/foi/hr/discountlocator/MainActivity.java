9ce4d46d113516f1d40bc6d6343187294130b895
Zlatko <zlatko.stapic@foi.hr>
2018-11-20 10:57:21 +0100

    05 Discount list: move data manipulation logic  - move all data manipulation logic from MainActivity to DiscountListFragment  - don't forget to make DiscountListFragment implement proper interface  - don't forget to fix context issue - fragment is not a context but can get its activity  - don't forget to organize imports in MainActivity

--- app/src/main/java/air18/foi/hr/discountlocator/MainActivity.java
+++ app/src/main/java/air18/foi/hr/discountlocator/MainActivity.java
@@ -2,39 +2,18 @@ package air18.foi.hr.discountlocator;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.air.ws.core.DataLoadedListener;
-import com.air.ws.core.DataLoader;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import air18.foi.hr.database.MainDatabase;
-import air18.foi.hr.database.entities.Discount;
-import air18.foi.hr.database.entities.Store;
-import air18.foi.hr.discountlocator.adapters.ExpandableStoreItem;
-import air18.foi.hr.discountlocator.adapters.StoreRecyclerAdapter;
 import air18.foi.hr.discountlocator.helper.Util;
-import air18.foi.hr.discountlocator.loaders.DbDataLoader;
-import air18.foi.hr.discountlocator.loaders.WsDataLoader;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements DataLoadedListener, SharedPreferences.OnSharedPreferenceChangeListener {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    StoreRecyclerAdapter mAdapter;
     private Util util = new Util();
 
     @Override
@@ -45,40 +24,8 @@ public class MainActivity extends AppCompatActivity implements DataLoadedListene
 
         util.setLanguage(this);
 
-        loadData();
         PreferenceManager.getDefaultSharedPreferences(this)
                 .registerOnSharedPreferenceChangeListener(this);
-    }
-
-
-    public void loadData(){
-        DataLoader dataLoader;
-        if(Store.getAll().isEmpty()){
-            System.out.println("Loading web data");
-            dataLoader = new WsDataLoader();
-        } else {
-            System.out.println("Loading local data");
-            dataLoader = new DbDataLoader();
-        }
-        dataLoader.loadData(this);
-    }
-
-    @Override
-    public void onDataLoaded(ArrayList<Store> stores, ArrayList<Discount> discounts) {
-        List<ExpandableStoreItem> storeItems = new ArrayList<>();
-
-        if(stores != null){
-
-            for(Store store : stores)
-                storeItems.add(new ExpandableStoreItem(store));
-
-            RecyclerView mRecycler = (RecyclerView) findViewById(R.id.main_recycler);
-            if(mRecycler != null){
-                mAdapter = new StoreRecyclerAdapter(this, storeItems);
-                mRecycler.setAdapter(mAdapter);
-                mRecycler.setLayoutManager(new LinearLayoutManager(this));
-            }
-        }
     }
 
     @Override
--- app/src/main/java/air18/foi/hr/discountlocator/fragments/DiscountListFragment.java
+++ app/src/main/java/air18/foi/hr/discountlocator/fragments/DiscountListFragment.java
@@ -4,13 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import air18.foi.hr.discountlocator.R;
+import com.air.ws.core.DataLoadedListener;
+import com.air.ws.core.DataLoader;
 
-public class DiscountListFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import air18.foi.hr.database.entities.Discount;
+import air18.foi.hr.database.entities.Store;
+import air18.foi.hr.discountlocator.R;
+import air18.foi.hr.discountlocator.adapters.ExpandableStoreItem;
+import air18.foi.hr.discountlocator.adapters.StoreRecyclerAdapter;
+import air18.foi.hr.discountlocator.loaders.DbDataLoader;
+import air18.foi.hr.discountlocator.loaders.WsDataLoader;
+
+public class DiscountListFragment extends Fragment implements DataLoadedListener {
+    StoreRecyclerAdapter mAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,5 +36,37 @@ public class DiscountListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        loadData();
+    }
+
+    public void loadData(){
+        DataLoader dataLoader;
+        if(Store.getAll().isEmpty()){
+            System.out.println("Loading web data");
+            dataLoader = new WsDataLoader();
+        } else {
+            System.out.println("Loading local data");
+            dataLoader = new DbDataLoader();
+        }
+        dataLoader.loadData(this);
+    }
+
+    @Override
+    public void onDataLoaded(ArrayList<Store> stores, ArrayList<Discount> discounts) {
+        List<ExpandableStoreItem> storeItems = new ArrayList<>();
+
+        if(stores != null){
+
+            for(Store store : stores)
+                storeItems.add(new ExpandableStoreItem(store));
+
+            RecyclerView mRecycler = (RecyclerView) getActivity().findViewById(R.id.main_recycler);
+            if(mRecycler != null){
+                mAdapter = new StoreRecyclerAdapter(getActivity(), storeItems);
+                mRecycler.setAdapter(mAdapter);
+                mRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+            }
+        }
     }
 }
