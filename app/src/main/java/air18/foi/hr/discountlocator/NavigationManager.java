e6580087907f90f6b618ed19d846e9c6d689a52f
Zlatko <zlatko.stapic@foi.hr>
2018-11-27 10:56:59 +0100

    06 Navigation manager: manage modules  - the logic of scanning and adding available modules to the list could be extended

--- app/src/main/java/air18/foi/hr/discountlocator/NavigationManager.java
+++ app/src/main/java/air18/foi/hr/discountlocator/NavigationManager.java
@@ -1,16 +1,28 @@
 package air18.foi.hr.discountlocator;
 
+import com.air.ws.core.NavigationItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import air18.foi.hr.discountlocator.fragments.DiscountListFragment;
+import air18.foi.hr.map.MapFragment;
+
 public class NavigationManager {
-    //TODO - manage modules
     //TODO - manage drawer
     //TODO - manage data
 
     //singleton
     private static NavigationManager instance;
 
+    //manage modules
+    private List<NavigationItem> navigationItems;
+
     private NavigationManager()
     {
-
+        navigationItems = new ArrayList<>();
+        navigationItems.add(new DiscountListFragment());
+        navigationItems.add(new MapFragment());
     }
 
     public static NavigationManager getInstance()
