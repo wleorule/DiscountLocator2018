7e0869e5c63551704ae96645eda7cbba92cde9c2
Zlatko <zlatko.stapic@foi.hr>
2018-11-27 10:53:06 +0100

    06 Modularity: morph MapFragment to fit interface  - add interface implementation  - implement methods  - use Alt+Enter and bind methods parameters to fields

--- map/src/main/java/air18/foi/hr/map/MapFragment.java
+++ map/src/main/java/air18/foi/hr/map/MapFragment.java
@@ -1,5 +1,7 @@
 package air18.foi.hr.map;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +10,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
+import com.air.ws.core.NavigationItem;
+
+import java.util.List;
+
+import air18.foi.hr.database.entities.Discount;
+import air18.foi.hr.database.entities.Store;
+
+public class MapFragment extends Fragment implements NavigationItem {
+    private List<Store> stores;
+    private List<Discount> discounts;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,5 +30,27 @@ public class MapFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public Fragment getFragment() {
+        return this;
+    }
+
+    @Override
+    public String getName(Context context) {
+        return context.getString(R.string.menu_map);
+    }
+
+    @Override
+    public Drawable getIcon(Context context) {
+        return context.getDrawable(android.R.drawable.ic_menu_mylocation);
+    }
+
+    @Override
+    public void setData(List<Store> stores, List<Discount> discounts) {
+
+        this.stores = stores;
+        this.discounts = discounts;
     }
 }
