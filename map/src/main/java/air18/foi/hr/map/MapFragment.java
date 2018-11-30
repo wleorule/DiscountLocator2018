10793cf7bcc00ccc98ad2bb90ea5e5a661df8a0c
Zlatko <zlatko.stapic@foi.hr>
2018-11-30 14:03:36 +0100

    06 Google maps: show the map  - get async map  - store the pointer to the map

--- map/src/main/java/air18/foi/hr/map/MapFragment.java
+++ map/src/main/java/air18/foi/hr/map/MapFragment.java
@@ -11,25 +11,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.air.ws.core.NavigationItem;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.List;
 
 import air18.foi.hr.database.entities.Discount;
 import air18.foi.hr.database.entities.Store;
 
-public class MapFragment extends Fragment implements NavigationItem {
+public class MapFragment extends Fragment implements NavigationItem, OnMapReadyCallback {
     private List<Store> stores;
     private List<Discount> discounts;
+    private GoogleMap mMap;
+    private com.google.android.gms.maps.SupportMapFragment mapFragment;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -52,5 +61,19 @@ public class MapFragment extends Fragment implements NavigationItem {
 
         this.stores = stores;
         this.discounts = discounts;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 }
